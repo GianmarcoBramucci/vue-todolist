76c0f7cc0bb2ab5cc5145a525bf0f72cfbf692e2
GianmarcoBramucci <giangian797@gmail.com>
2023-09-04 09:30:52 +0200

    Melstrone1

--- js/script.js
+++ js/script.js
@@ -2,9 +2,26 @@ const { createApp } = Vue
         createApp({
             data(){
                 return{
-                
-                }
-            }
-        }).mount('#app')
+                    tobooList:[
+                        {
+                            done: true,
+                            title: 'New Task',
+                            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptatum eos a quos?',
+                        }, 
+                        {
+                            done: true,
+                            title: 'Today',
+                            text: 'Lorem ipsum dolor sit amet.',
+                        }, 
+                        {
+                            done: true,
+                            title: 'Upcoming',
+                            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore?',
+                        }
+                        ]
+                            }
+                        }
+                    }).mount('#app')
 
 
+                    
