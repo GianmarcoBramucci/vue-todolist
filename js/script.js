d4ff1c6406e1fc7b2f82b1c2993b22cd2b6d0ba6
GianmarcoBramucci <giangian797@gmail.com>
2023-09-06 18:53:08 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -18,7 +18,31 @@ const { createApp } = Vue
                             title: 'Upcoming',
                             text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore?',
                         }
-                        ]
+                        ],
+                        addNewTask:{
+                                    done: false,
+                                    title: 'new task',
+                                    text : ''
+                        }
+
+                            }
+                        },
+                        methods:{
+                            deleteTobo(index){
+                                this.tobooList.splice(index,1)
+                            },
+                            toogle(index){
+                                this.tobooList[index].done=!this.tobooList[index].done;
+                            }, 
+                            addTask(newTask,addNewTask,tobooList){
+                                addNewTask={
+                                    done: false,
+                                    title: 'new task',
+                                    text : newTask
+                                }
+                                tobooList.push(addNewTask)
+                                console.log(tobooList)
+                                newTask=''
                             }
                         }
                     }).mount('#app')
