04feed9e5fd74a1193cc94ac4142816eab0a7264
GianmarcoBramucci <giangian797@gmail.com>
2024-04-02 15:33:55 +0100

    setup

---
+++ js/data.js
@@ -0,0 +1,28 @@
+
+export  const todoList = [
+    {
+        id: 1,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, enim!',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia facere explicabo qui eius deleniti commodi.',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ratione quos et odio laboriosam nulla. Quaerat, maxime.',
+        done: false
+    },
+    {
+        id: 4,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate voluptatum eveniet voluptates at?',
+        done: false
+    },
+    {
+        id: 5,
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe natus eum quae.',
+        done: false
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import {todoList} from "./data.js"; 
+
+Vue.createApp({
+    data(){
+        return{
+            todoList,
+        }
+    },
+    methods: {
+
+    },
+    mounted(){
+       
+    }
+}).mount('#app')
